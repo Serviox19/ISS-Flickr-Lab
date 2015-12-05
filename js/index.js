8175544a3ca66424d0051c4337b984e51d542a94
Serviox19 <servio19@gmail.com>
2015-12-05 14:05:32 -0500

    added layout and initial javascript

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("#submitButton").on("click", function(e) {
+    e.preventDefault();
+    var address = $("#enterAddress").val();
+    
+    $ajax
+
+
+  });
+
+    
+});
