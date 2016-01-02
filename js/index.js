66d0844b91d0888afde22551c3a7b6205bf7b0ad
Serviox19 <servio19@gmail.com>
2016-01-01 21:51:21 -0500

    able to get geocode from address and pop it into iss ajax request

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
-
-  $("#submitButton").on("click", function(e) {
+  $("#submitButton").on("click", function (e) {
     e.preventDefault();
-    var address = $("#enterAddress").val();
-    
-    $ajax
 
+    var userAddress = $("#enterAddress").val();
+    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+    var apiKey = "AIzaSyAbGfA36DC0Yp6RXDLuR-lInAIKfgquxy0"
+    googleApiUrl += "address=" + userAddress;
+    googleApiUrl += "&key=" + apiKey;
+
+    $.ajax({
+      type: "GET",
+      url: googleApiUrl,
+      success: function (response) {
+        var geoLocation = response.results[0].geometry.location;
+        var geoLat = geoLocation.lat
+        var geoLon = geoLocation.lng
+
+        var issUrl = "http://api.open-notify.org/iss-pass.json?";
+        issUrl += "lat=" + geoLat;
+        issUrl += "&lon=" + geoLon;
+        issUrl += "&callback=CALLBACK";
+
+        $.ajax({
+          type: "GET",
+          url: issUrl,
+          success: function (data) {
+            console.log(data.response);
+          }
+        });
+      }
+    });
 
   });
-
-    
 });
